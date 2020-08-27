5558efedeb594f69ef82f5520c36301704d48271
canremzi80 <70340285+canremzi80@users.noreply.github.com>
2020-08-27 21:00:58 +0300

    Add files via upload

---
+++ html/locales/en.js
@@ -0,0 +1,5 @@
+var invLocale = new Object();
+invLocale.dropItem = "At";
+invLocale.useItem = "Kullan";
+invLocale.giveItem = "Ver";
+invLocale.secondInventoryNotAvailable = "İkinci envanter aktif değil";
